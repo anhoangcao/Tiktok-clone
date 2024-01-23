7561e6b32faeb4ef760eb8c94d923cdeccacd61a
HoangAn-B2003774 <115530751+HoangAn-B2003774@users.noreply.github.com>
2024-01-23 19:20:55 +0700

    update

--- app/layouts/includes/MenuItem.tsx
+++ app/layouts/includes/MenuItem.tsx
@@ -1,17 +1,18 @@
 "use client"
 
-import { MenuItemsTypes } from "@/app/types";
-import { AiOutlineHome } from "react-icons/ai";
-import { BsCameraVideo } from "react-icons/bs";
+import { AiOutlineHome } from "react-icons/ai"
 import { RiGroupLine } from "react-icons/ri"
+import { BsCameraVideo } from "react-icons/bs"
+import { MenuItemTypes } from "@/app/types"
 
-export default function MenuItem({ iconString, colorString, sizeString }: MenuItemsTypes) {
+export default function MenuItem({ iconString, colorString, sizeString }: MenuItemTypes) {
 
     const icons = () => {
         if (iconString == 'For You') return <AiOutlineHome size={sizeString} color={colorString} />
         if (iconString == 'Following') return <RiGroupLine size={sizeString} color={colorString} />
         if (iconString == 'LIVE') return <BsCameraVideo size={sizeString} color={colorString} />
     }
+
     return (
         <>
             <div className="w-full flex items-center hover:bg-gray-100 p-2.5 rounded-md">
